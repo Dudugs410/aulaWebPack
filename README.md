78e63c2c67b266a724187117114a1347e69918ad
Dudugs410 <dudugs_@hotmail.com>
2022-10-29 11:22:48 -0300

    sum function added

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto de Aula Revisão
+
+## Pré Requisitos
+- Ter o NodeJS 16 instalado na máquina
+
+## Tecnologias
+
+-NodeJS
+-SASS
--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log("Olá JS")
+
+function sum( num_01, num_02){
+
+    var sum = parseInt(num_01) + parseInt(num_02)
+    return sum;
+}
+
+
+function executeSum(){
+    
+    var num_01 = document.querySelector("#num_01").value
+    var num_02 = document.querySelector("#num_02").value
+    
+    if((isNaN(num_01)) || (isNaN(num_02)))
+    {
+        alert("Type a valid number")
+        return
+    }
+    
+    const resultado = sum(num_01, num_02)
+
+    document.querySelector("#resultSum").innerHTML = resultado
+
+}
